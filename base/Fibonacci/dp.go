d1faac14370463c5e48bfabb49375550391c6b61
gaozhenan <gaozhenan@sogou-inc.com>
2020-03-01 11:24:40 +0800

    update...........

--- base/Fibonacci/dp.go
+++ base/Fibonacci/dp.go
@@ -9,6 +9,35 @@ func main() {
 	//fmt.Println(F(2))
 	fmt.Println("F", F(6))
 	fmt.Println("F2", F2(6))
+	fmt.Println("F3", F3(6))
+}
+
+func F(i int) (f int) {
+	if i <= 0 {
+		return 0
+	}
+	mem := make([]int, i+1)
+	for k := 0; k < i+1; k++ {
+		mem[k] = -1
+	}
+	//fmt.Println(mem)
+
+	return dp(i, mem)
+}
+
+func dp(i int, mem []int) int {
+	if mem[i] != -1 {
+		//fmt.Println("节省次数", mem, "len", len(mem), "i:", i)
+		return mem[i]
+	}
+	if i <= 2 {
+		mem[i] = 1
+	} else {
+		mem[i] = dp(i-1, mem) + dp(i-2, mem)
+	}
+
+	//fmt.Println(mem, "len", len(mem), "i:", i)
+	return mem[i]
 }
 
 func F2(n int) (f int) {
@@ -31,30 +60,23 @@ func F2(n int) (f int) {
 
 }
 
-func F(i int) (f int) {
-	if i <= 0 {
+func F3(n int) (f int) {
+	if n <= 0 {
 		return 0
 	}
-	mem := make([]int, i+1)
-	for k := 0; k < i+1; k++ {
-		mem[k] = -1
+	if n == 1 || n == 2 {
+		return 1
 	}
-	fmt.Println(mem)
+	a_2 := 0
+	a_1 := 1
+	a_i := 1
 
-	return dp(i, mem)
-}
-
-func dp(i int, mem []int) int {
-	if mem[i] != -1 {
-		fmt.Println("节省次数", mem, "len", len(mem), "i:", i)
-		return mem[i]
-	}
-	if i <= 2 {
-		mem[i] = 1
-	} else {
-		mem[i] = dp(i-1, mem) + dp(i-2, mem)
+	for i := 2; i <= n; i++ {
+		a_i = a_1 + a_2
+		a_2 = a_1
+		a_1 = a_i
 	}
 
-	fmt.Println(mem, "len", len(mem), "i:", i)
-	return mem[i]
+	return a_i
+
 }
